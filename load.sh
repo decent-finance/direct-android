e1f70d51d2eab7d83c51c054809509d0b7d21516
Sergei Munovarov <serhuz@ymail.com>
2019-07-31 17:25:34 +0300

    Add Circle CI config

---
+++ load.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CONFIG=${HOME}/direct/config.properties
+export CONFIG
+
+if [ ! -f "$CONFIG" ]; then
+    touch $CONFIG
+
+    echo "devRestUrl=$DEV_REST_URL" >> ${CONFIG}
+    echo "devWsUrl=$DEV_WS_URL" >> ${CONFIG}
+    echo "uatRestUrl=$UAT_REST_URL" >> ${CONFIG}
+    echo "uatWsUrl=$UAT_WS_URL" >> ${CONFIG}
+    echo "devPlacementId=$DEV_PLACEMENT_ID" >> ${CONFIG}
+    echo "devSecret=$DEV_SECRET" >> ${CONFIG}
+    echo "uatPlacementId=$UAT_PLACEMENT_ID" >> ${CONFIG}
+    echo "uatSecret=$UAT_SECRET" >> ${CONFIG}
+    echo "prodPlacementId=$PROD_PLACEMENT_ID" >> ${CONFIG}
+    echo "prodSecret=$PROD_SECRET" >> ${CONFIG}
+
+fi
